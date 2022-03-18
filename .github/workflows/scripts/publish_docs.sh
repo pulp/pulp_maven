604329b2e66b68acf1c08829f20400c4edf511de
pulpbot <pulp-infra@redhat.com>
2022-03-18 14:21:35 +0000

    Update CI files
    [noissue]

--- .github/workflows/scripts/publish_docs.sh
+++ .github/workflows/scripts/publish_docs.sh
@@ -31,3 +31,42 @@ eval "$(ssh-agent -s)" #start the ssh agent
 ssh-add ~/.ssh/pulp-infra
 
 python3 .github/workflows/scripts/docs-publisher.py --build-type $1 --branch $2
+
+if [[ "$GITHUB_WORKFLOW" == "Maven changelog update" ]]; then
+  # Do not build bindings docs on changelog update
+  exit
+fi
+
+# Building python bindings
+export PULP_URL="${PULP_URL:-https://pulp}"
+VERSION=$(http $PULP_URL/pulp/api/v3/status/ | jq --arg plugin maven --arg legacy_plugin pulp_maven -r '.versions[] | select(.component == $plugin or .component == $legacy_plugin) | .version')
+cd ../pulp-openapi-generator
+rm -rf pulp_maven-client
+./generate.sh pulp_maven python $VERSION
+cd pulp_maven-client
+
+# Adding mkdocs
+cp README.md docs/index.md
+sed -i 's/docs\///g' docs/index.md
+sed -i 's/\.md//g' docs/index.md
+cat >> mkdocs.yml << DOCSYAML
+---
+site_name: PulpMaven Client
+site_description: Maven bindings
+site_author: Pulp Team
+site_url: https://docs.pulpproject.org/pulp_maven_client/
+repo_name: pulp/pulp_maven
+repo_url: https://github.com/pulp/pulp_maven
+theme: readthedocs
+DOCSYAML
+
+pip install mkdocs pymdown-extensions
+
+# Building the bindings docs
+mkdocs build
+
+# publish to docs.pulpproject.org/pulp_maven_client
+rsync -avzh site/ doc_builder_pulp_maven@docs.pulpproject.org:/var/www/docs.pulpproject.org/pulp_maven_client/
+
+# publish to docs.pulpproject.org/pulp_maven_client/en/{release}
+rsync -avzh site/ doc_builder_pulp_maven@docs.pulpproject.org:/var/www/docs.pulpproject.org/pulp_maven_client/en/"$1"
