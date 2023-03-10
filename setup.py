714acf1f1b152338d8976a500733f8bca498dc3d
Dennis Kliban <dkliban@redhat.com>
2023-03-10 16:36:18 -0500

    Releasing 0.4.0
    [noissue]

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("requirements.txt") as requirements:
 
 setup(
     name="pulp-maven",
-    version="0.4.0.dev",
+    version="0.4.0",
     description="pulp-maven plugin for the Pulp Project",
     license="GPLv2+",
     author="Pulp Project Developers",
