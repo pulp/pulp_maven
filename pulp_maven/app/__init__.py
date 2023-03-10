e1eda26bb61caa5ac43a8332928d796ce1c0314c
Dennis Kliban <dkliban@redhat.com>
2023-03-10 16:37:10 -0500

    Bump version to 0.5.0.dev
    [noissue]

--- pulp_maven/app/__init__.py
+++ pulp_maven/app/__init__.py
@@ -6,5 +6,5 @@ class PulpMavenPluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_maven.app"
     label = "maven"
-    version = "0.4.0"
+    version = "0.5.0.dev"
     python_package_name = "pulp-maven"
