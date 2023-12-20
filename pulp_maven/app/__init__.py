b23042ef7b3c23ebd0b9ac5d424f785340d40edb
pulpbot <pulp-infra@redhat.com>
2023-12-20 14:01:52 +0000

    Bump to 0.8.1.dev
    [noissue]

--- pulp_maven/app/__init__.py
+++ pulp_maven/app/__init__.py
@@ -6,5 +6,5 @@ class PulpMavenPluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_maven.app"
     label = "maven"
-    version = "0.8.0"
+    version = "0.8.1.dev"
     python_package_name = "pulp-maven"
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("requirements.txt") as requirements:
 
 setup(
     name="pulp-maven",
-    version="0.8.0",
+    version="0.8.1.dev",
     description="pulp-maven plugin for the Pulp Project",
     license="GPLv2+",
     author="Pulp Project Developers",
