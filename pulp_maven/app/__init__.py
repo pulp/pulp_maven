6134544aa3a267cf3a9721ff1c9f0c8c8c1676bc
pulpbot <pulp-infra@redhat.com>
2025-03-31 19:39:59 +0000

    Bump version: 0.10.0 → 0.10.1.dev

--- pulp_maven/app/__init__.py
+++ pulp_maven/app/__init__.py
@@ -6,6 +6,6 @@ class PulpMavenPluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_maven.app"
     label = "maven"
-    version = "0.10.0"
+    version = "0.10.1.dev"
     python_package_name = "pulp-maven"
     domain_compatible = True
