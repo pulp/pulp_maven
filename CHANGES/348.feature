99e8fec61ad10ff622000c0112d392d05751dee3
pulpbot <pulp-infra@redhat.com>
2026-06-26 03:02:40 +0000

    Bump minor version

--- CHANGES/348.feature
+++
@@ -1,1 +0,0 @@
-The Maven Artifact create API now accepts file uploads in addition to artifact hrefs.
--- pulp_maven/app/__init__.py
+++ pulp_maven/app/__init__.py
@@ -6,6 +6,6 @@ class PulpMavenPluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_maven.app"
     label = "maven"
-    version = "0.15.0.dev"
+    version = "0.16.0.dev"
     python_package_name = "pulp-maven"
     domain_compatible = True
