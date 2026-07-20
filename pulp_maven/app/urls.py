5bd6a7d1dbdbd7e3de4f10265199aaac649f0541
Carlos Feria <2582866+carlosthe19916@users.noreply.github.com>
2026-07-20 13:06:48 +0200

    fix: formatting

--- pulp_maven/app/urls.py
+++ pulp_maven/app/urls.py
@@ -5,23 +5,21 @@ from pulp_maven.app.maven_deploy_api import MavenApiViewSet
 from pulp_maven.app.simple.views import MetadataView, SimpleView
 
 if settings.DOMAIN_ENABLED:
-    deploy_re = r"(?P<pulp_domain>[-a-zA-Z0-9_]+)/(?P<name>[\w-]+)/(?P<path>.*)"
+    path_re = r"(?P<pulp_domain>[-a-zA-Z0-9_]+)/(?P<name>[\w-]+)/(?P<path>.*)"
     MAVEN_API_URL = "/<slug:pulp_domain>/<path:path>/"
 else:
-    deploy_re = r"(?P<name>[\w-]+)/(?P<path>.*)"
+    path_re = r"(?P<name>[\w-]+)/(?P<path>.*)"
     MAVEN_API_URL = "/<path:path>/"
+
 MAVEN_API_URL = settings.MAVEN_PATH_PREFIX.strip("/") + MAVEN_API_URL
 
 urlpatterns = [
-    # Deploy API (existing)
-    re_path(rf"^pulp/maven/{deploy_re}$", MavenApiViewSet.as_view()),
-    # Metadata API
+    re_path(rf"^pulp/maven/{path_re}$", MavenApiViewSet.as_view()),
     path(
         MAVEN_API_URL + "maven/<path:meta>/",
         MetadataView.as_view({"get": "retrieve"}),
         name="maven-metadata",
     ),
-    # Simple API
     path(
         MAVEN_API_URL + "simple/",
         SimpleView.as_view({"get": "list"}),
